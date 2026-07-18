625f5df834a43afaaacc6f1bdbd3f984acd58c07
Monutzzz <ramon.marinez.1@gmail.com>
2026-07-17 23:46:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ---- Supabase setup ----
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://hmxjngqxmesqixuxmoel.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_MyYPj-1x2dhgEOUASP50bA_UYO6mQwk';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const syncDot = document.getElementById('syncDot');
